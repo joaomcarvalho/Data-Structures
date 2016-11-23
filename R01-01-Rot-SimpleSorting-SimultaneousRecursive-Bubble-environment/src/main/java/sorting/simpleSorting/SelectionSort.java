52e55c8a6e24a53ffba5c2f4eb4a9fe265bf65a6
JOAO MAURICIO ALVES VALVERDE CARVALHO 115211593 <joao.carvalho@ccc.ufcg.edu.br>
2016-11-23 17:09:55 -0300

    eu to com sono

--- R01-01-Rot-SimpleSorting-SimultaneousRecursive-Bubble-environment/src/main/java/sorting/simpleSorting/SelectionSort.java
+++ R01-01-Rot-SimpleSorting-SimultaneousRecursive-Bubble-environment/src/main/java/sorting/simpleSorting/SelectionSort.java
@@ -1,6 +1,7 @@
 package sorting.simpleSorting;
 
 import sorting.AbstractSorting;
+import util.Util;
 
 /**
  * The selection sort algorithm chooses the smallest element from the array and
@@ -11,7 +12,16 @@ public class SelectionSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		int minIdex, j;
+		for (int i = leftIndex; i <= rightIndex; i++){
+			minIdex = i;
+			for (j = i + 1; j <= rightIndex; j++){
+				if (array[j].compareTo(array[minIdex]) < 0 ){
+					minIdex = j;
+				}
+			}
+			if (minIdex != i)
+				Util.swap(array, i, minIdex);
+		}
 	}
 }
