86ba68af18951c72f16288e2da189c47b1960ddd
Joao Mauricio <joao.carvalho@ccc.ufcg.edu.br>
2016-11-30 23:12:00 -0300

    opa

--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
@@ -10,38 +10,45 @@ import sorting.AbstractSorting;
  */
 public class CountingSort extends AbstractSorting<Integer> {
 
-	@Override
-	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		if (array.length == 0)
-			return;
-		
-		Integer maior = array[leftIndex];
-		for (int i = leftIndex + 1; i <= rightIndex; i++){
-			if (array[i].compareTo(maior) > 0){
-				maior = array[i];
-			}
-		}
-		
-		int[] freq = new int[maior + 1];
-		
-		for (int i = leftIndex; i <= rightIndex; i++){
-			freq[array[i]]++;
-		}
-		
-		for (int i = 1; i < freq.length; i++){
-			freq[i] += freq[i - 1];
-		}
-		
-		Integer[] b = new Integer [rightIndex + 1];
-		
-		for (int i = rightIndex; i >= leftIndex; i--){
-			b[freq[array[i]]-1] = array[i];
-			freq[array[i]]--;
-		}
-		
-		for (int i = leftIndex; i <= rightIndex; i++){
-			array[i] = b[i];
-		}
-	}
+   @Override
+   public void sort(Integer[] array, int leftIndex, int rightIndex) {
+      if (array == null || array.length == 0)
+         return;
+      if (leftIndex < 0)
+         leftIndex = 0;
+      if (rightIndex > array.length - 1)
+         rightIndex = array.length - 1;
+      if (leftIndex > rightIndex)
+         return;
 
+      Integer maior = array[leftIndex];
+      for (int i = leftIndex + 1; i <= rightIndex; i++) {
+         if (array[i].compareTo(maior) > 0) {
+            maior = array[i];
+         }
+      }
+
+      int[] freq = new int[maior + 1];
+
+      for (int i = leftIndex; i <= rightIndex; i++) {
+         freq[array[i]]++;
+      }
+
+      for (int i = 1; i < freq.length; i++) {
+         freq[i] += freq[i - 1];
+      }
+
+      Integer[] b = new Integer[rightIndex - leftIndex + 1];
+
+      for (int i = rightIndex; i >= leftIndex; i--) {
+         b[freq[array[i]] - 1] = array[i];
+         freq[array[i]]--;
+      }
+
+      int j = 0;
+      for (int i = leftIndex; i <= rightIndex; i++) {
+         array[i] = b[j];
+         j++;
+      }
+   }
 }
--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -12,8 +12,47 @@ public class ExtendedCountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (array == null || array.length == 0)
+			return;
+		if (leftIndex < 0)
+			leftIndex = 0;
+		if (rightIndex > array.length - 1)
+			rightIndex = array.length - 1;
+		if (leftIndex > rightIndex)
+			return;
+		
+		Integer maior = array[leftIndex];
+		Integer menor = array[leftIndex];
+		
+		for (int i = leftIndex + 1; i <= rightIndex; i++){
+			if (array[i].compareTo(maior) > 0)
+				maior = array[i];
+			if (array[i].compareTo(menor) < 0)
+				menor = array[i];
+		}
+		
+		
+		int freq[] = new int[maior - menor + 1];
+		
+		for (int i = leftIndex; i <= rightIndex; i++){
+			freq[array[i] - menor]++;
+		}
+		
+		for (int i = 1; i < freq.length; i++){
+			freq[i] += freq[i - 1];
+		}
+		
+		Integer[] b = new Integer[rightIndex - leftIndex + 1];
+		
+		for (int i = leftIndex; i <= rightIndex; i++){
+			b[freq[array[i] - menor] - 1] = array[i];
+			freq[array[i] - menor]--;
+		}
+		
+		int j = 0;
+		for (int i = leftIndex; i <= rightIndex; i++){
+			array[i] = b[j];
+			j++;
+		}
 	}
-
 }
