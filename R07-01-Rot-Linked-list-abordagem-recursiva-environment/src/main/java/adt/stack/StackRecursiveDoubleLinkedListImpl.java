e7d5054636311365fd7e157df6cd197ea66a86f6
Joao Mauricio <joao.carvalho@ccc.ufcg.edu.br>
2016-12-19 18:43:22 -0300

    ultimo commit do dia

--- R07-01-Rot-Linked-list-abordagem-recursiva-environment/src/main/java/adt/stack/StackRecursiveDoubleLinkedListImpl.java
+++ R07-01-Rot-Linked-list-abordagem-recursiva-environment/src/main/java/adt/stack/StackRecursiveDoubleLinkedListImpl.java
@@ -2,48 +2,49 @@ package adt.stack;
 
 import adt.linkedList.DoubleLinkedList;
 import adt.linkedList.RecursiveDoubleLinkedListImpl;
-import adt.linkedList.RecursiveSingleLinkedListImpl;
 
 public class StackRecursiveDoubleLinkedListImpl<T> implements Stack<T> {
 
-	protected DoubleLinkedList<T> top;
-	protected int size;
+   protected DoubleLinkedList<T> top;
+   protected int size;
 
-	public StackRecursiveDoubleLinkedListImpl(int size) {
-		this.size = size;
-		this.top = new RecursiveDoubleLinkedListImpl<T>();
-	}
+   public StackRecursiveDoubleLinkedListImpl(int size) {
+      this.size = size;
+      this.top = new RecursiveDoubleLinkedListImpl<T>();
+   }
 
-	@Override
-	public void push(T element) throws StackOverflowException {
-		if (element == null) return;
-		if (isFull()) throw new StackOverflowException();
-		
-		top.insertFirst(element);
-		size--;
-	}
+   @Override
+   public void push(T element) throws StackOverflowException {
+      if (element == null)
+         return;
+      if (isFull())
+         throw new StackOverflowException();
 
-	@Override
-	public T pop() throws StackUnderflowException {
-		T element = ((RecursiveDoubleLinkedListImpl<T>) top).getData();
-		top.removeFirst();
-		size++;
-		return element;
-	}
+      top.insertFirst(element);
+      size--;
+   }
 
-	@Override
-	public T top() {
-		return ((RecursiveDoubleLinkedListImpl<T>) top).getData();
-	}
+   @Override
+   public T pop() throws StackUnderflowException {
+      T element = ((RecursiveDoubleLinkedListImpl<T>) top).getData();
+      top.removeFirst();
+      size++;
+      return element;
+   }
 
-	@Override
-	public boolean isEmpty() {
-		return top.isEmpty();
-	}
+   @Override
+   public T top() {
+      return ((RecursiveDoubleLinkedListImpl<T>) top).getData();
+   }
 
-	@Override
-	public boolean isFull() {
-		return size == 0;
-	}
+   @Override
+   public boolean isEmpty() {
+      return top.isEmpty();
+   }
+
+   @Override
+   public boolean isFull() {
+      return size == 0;
+   }
 
 }
